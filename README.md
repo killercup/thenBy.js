ea5e2d6e2ea28d04e9fa52944d4ed356bdedede3
Pascal Hertleif <killercup@gmail.com>
2015-01-20 12:07:05 +0100

    Convert to CommonJS, Add Property Shorthand

--- README.md
+++ README.md
@@ -3,17 +3,34 @@ thenBy.js
 
 `thenBy` is a javascript micro library that helps sorting arrays on multiple keys. It allows you to use the [native Array::sort() method](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) of javascript, but pass in multiple functions to sort that are composed with `firstBy().thenBy().thenBy()` style.
 
-Example:
+The original version was written by Teun Duynstee.
+
+## Install
+
+```sh
+npm install --save killercup/thenBy.js
+```
+
+```js
+var firstBy = require('then-by');
+```
+
+## Example:
+
 ```javascript
 // first by length of name, then by population, then by ID
 data.sort(
-    firstBy(function (v1, v2) { return v1.name.length - v2.name.length; })
-    .thenBy(function (v1, v2) { return v1.population - v2.population; })
-    .thenBy(function (v1, v2) { return v1.id - v2.id; })
+  firstBy(function (v1, v2) { return v1.name.length - v2.name.length; })
+  .thenBy(function (v1, v2) { return v1.population - v2.population; })
+  .thenBy(function (v1, v2) { return v1.id - v2.id; })
 );
 
+// Using property keys
+data.sort(
+  firstBy(function (v1, v2) { return v1.name.length - v2.name.length; })
+  .thenBy('population')
+  .thenBy('id')
+);
 ```
-
-To include it into your page/project, just paste the minified code from https://raw.github.com/Teun/thenBy.js/master/thenBy.min.js into yours (137 characters). 
 
 Thanks a lot to https://github.com/bergus for his improvements.
--- thenBy.js
+++ thenBy.js
@@ -1,32 +1,51 @@
-/***
-   Copyright 2013 Teun Duynstee
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+/**
+ * Copyright 2013 Teun Duynstee
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @license
+ *
+ * Updated 2015 by Pascal Hertleif for CommonJS support and property shorthand.
  */
-firstBy = (function() {
-    /* mixin for the `thenBy` property */
-    function extend(f) {
-        f.thenBy = tb;
-        return f;
-    }
-    /* adds a secondary compare function to the target function (`this` context)
-       which is applied in case the first one returns 0 (equal)
-       returns a new compare function, which has a `thenBy` method as well */
-    function tb(y) {
-        var x = this;
-        return extend(function(a, b) {
-            return x(a,b) || y(a,b);
-        });
-    }
-    return extend;
-})();
+
+function compareProp(c) {
+  if (typeof c === 'function') { return c; }
+
+  return function (v1, v2) { return v1[c] - v2[c]; };
+}
+
+var extend, tb;
+
+/*
+ * Mixin for the `thenBy` property
+ */
+extend = function extendThenBy(c) {
+  var fn = compareProp(c);
+
+  fn.thenBy = tb;
+  return fn;
+};
+
+/*
+ * Adds a secondary compare function to the target function (`this` context)
+ * which is applied in case the first one returns 0 (equal) returns a new
+ * compare function, which has a `thenBy` method as well
+ */
+tb = function thenBy(c) {
+  var x = this;
+  var y = compareProp(c);
+  return extend(function (a, b) {
+    return x(a, b) || y(a, b);
+  });
+};
+
+module.exports = extend;
